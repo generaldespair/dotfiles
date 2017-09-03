21bfa63fe66fd05b598dbfcc85f6babcb5d41b78
Joe <generaldespair@gmail.com>
2017-09-02 21:38:06 -0400

    Initial commit

---
+++ .config/plasma-workspace/env/wm.sh
@@ -0,0 +1,7 @@
+# Disable KWin and use i3gaps as WM
+export KDEWM=/usr/bin/i3
+#export KDEWM=/usr/bin/kwin_x11
+# Compositor (Animations, Shadows, Transparency)
+# xcompmgr -C
+#compton -cCFb --backend glx --vsync opengl
+
